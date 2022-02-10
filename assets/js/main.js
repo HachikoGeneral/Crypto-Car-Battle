50f95d7a86d048fb2f49acad53da12188c3214b0
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-10 12:28:46 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ game = new Phaser.Game(1280, 839, Phaser.AUTO, "");
 
 game.state.add("Login", Login);
 game.state.add("Mint", Mint);
-game.state.add("CarsListLoader", CarsListLoader);
 game.state.add("CarsList", CarsList);
 game.state.add("ChallengeChooser", ChallengeChooser);
 game.state.add("ChallengesListLoader", ChallengesListLoader);
