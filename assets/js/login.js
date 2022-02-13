4d79fc01bd279f4b2519a052aa1ec0cbc1a218f9
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 02:15:23 +0000

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,7 +16,7 @@ var Login = {
   },
 
   web3login: function () {
-    contract.properties
+    contract.undefined
       .balanceOf(web3.eth.defaultAccount)
       .call()
       .then(function (res) {
