bbde7df61cb2ccb4fbda8a556946bb9a26088bb7
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 10:53:29 +0000

    Update web3setup.js

--- assets/js/web3setup.js
+++ assets/js/web3setup.js
@@ -37,9 +37,3 @@ web3.eth.getAccounts((err, res) => {
 });
 
 var contract = new web3.eth.Contract(abi, caddress);
-
-{
-    // Use IntelliSense to learn about possible attributes.
-    // Hover to view descriptions of existing attributes.
-    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-   
