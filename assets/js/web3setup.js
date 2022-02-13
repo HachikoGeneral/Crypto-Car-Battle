809d19b77392059a5bbb4b3630110f90fe319a6a
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 11:29:25 +0000

    Update web3setup.js

--- assets/js/web3setup.js
+++ assets/js/web3setup.js
@@ -40,7 +40,7 @@ var contract = new web3.eth.Contract(abi, caddress);
     // Use IntelliSense to learn about possible attributes.
     // Hover to view descriptions of existing attributes.
     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-    "version": "0.1.0",
+    "0.1.0",
     "configurations": [
         {
             "name": "Launch Chrome",
