a715aa218b2a988ef1a49ef0a13903e8656064db
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 18:51:40 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ var NFT_balance;
 //new game instance
 game = new Phaser.Game(1280, 839, Phaser.AUTO, "");
 
-game = ("Login", Login);
-game = ("Mint", Mint);
-game = ("CarsListLoader", CarsListLoader);
-game = ("CarsList", CarsList);
-game = ("ChallengeChooser", ChallengeChooser);
-game = ("ChallengesListLoader", ChallengesListLoader);
-game = ("ChallengesList", ChallengesList);
-game = ("Game", Game);
-game = ("OpenForBattle", OpenForBattle);
+game.state = ("Login", Login);
+game.state = ("Mint", Mint);
+game.state = ("CarsListLoader", CarsListLoader);
+game.state = ("CarsList", CarsList);
+game.state = ("ChallengeChooser", ChallengeChooser);
+game.state = ("ChallengesListLoader", ChallengesListLoader);
+game.state = ("ChallengesList", ChallengesList);
+game.state = ("Game", Game);
+game.state = ("OpenForBattle", OpenForBattle);
 
-game = ("Login");
+game.start = ("Login");
