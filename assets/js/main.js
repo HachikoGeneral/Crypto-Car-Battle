5c6654b089c17f114ef70d8910d75f982e4c4583
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 21:36:05 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,14 @@ var add=99;    //initialize here
 console.log(myFunc(add));
 game = new Phaser.Game(1280, 839, Phaser.AUTO, "")
 
-game.state.add = ("Login"),
-game.state.add = ("Mint", Mint),
-game.state.add = ("CarsListLoader", CarsListLoader),
-game.state.add = ("CarsList", CarsList),
-game.state.add = ("ChallengeChooser", ChallengeChooser),
-game.state.add = ("ChallengesListLoader", ChallengesListLoader),
-game.state.add = ("ChallengesList", ChallengesList),
-game.state.add = ("Game", Game),
-game.state.add = ("OpenForBattle", OpenForBattle),
+game.state.add("Login", Login);
+game.state.add("Mint", Mint);
+game.state.add("CarsListLoader", CarsListLoader);
+game.state.add("CarsList", CarsList);
+game.state.add("ChallengeChooser", ChallengeChooser);
+game.state.add("ChallengesListLoader", ChallengesListLoader);
+game.state.add("ChallengesList", ChallengesList);
+game.state.add("Game", Game);
+game.state.add("OpenForBattle", OpenForBattle);
 
-game.state.start = ("boot")
+game.state.start = ("boot");
