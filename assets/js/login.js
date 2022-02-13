245e46dd1300dc9da32e21c0dfe60f904f10df45
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-13 14:09:48 +0000

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,10 +16,6 @@ var Login = {
   },
 
   web3login: function () {
-    var caddress = "0xa5bf757Ea56bD4d81fBD68CC6dfAb5C4eC139500";
-      .balanceOf(web3.eth.defaultAccount)
-      .call()
-      .then(function (res) {
         console.log(res, "BALANCE");
         NFT_balance = res;
         if(NFT_balance == 0) {
