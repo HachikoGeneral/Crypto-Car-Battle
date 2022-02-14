003b67cd0dce55ff0732ca5c134eff91f7a75705
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-14 00:20:15 +0000

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,9 +16,58 @@ var Login = {
   },
 
   web3login: function () {
-    console.log(res, "BALANCE");
+    contract.methods
+      .balanceOf(web3.eth.defaultAccount)
+      .call()
+      .then(function (res) {
+        console.log(res, "BALANCE");
         NFT_balance = res;
         if(NFT_balance == 0) {
           game.state.start('Mint');
         } else {
           game.state.start('CarsListLoader')
+
+          var tokenCount;
+var userCars = [];
+var selectedCar;
+
+var carLevelWithColor = {
+  1: "violet",
+  2: "indigo",
+  3: "blue",
+  4: "green",
+  5: "yellow",
+  6: "orange",
+  7: "red",
+};
+
+var CarsListLoader = {
+  preload: function () {
+    contract.methods
+      .balanceOf(web3.eth.defaultAccount)
+      .call()
+      .then(function (res) {
+        tokenCount = res;
+        for (var i = 0; i < tokenCount; i++) {
+          contract.methods
+            .tokenOfOwnerByIndex(web3.eth.defaultAccount, i)
+            .call()
+            .then(function (index) {
+              contract.methods
+                .cars(index)
+                .call()
+                .then(function (car) {
+                  userCars.push(car);
+                });
+            });
+        }
+      });
+  },
+
+  update: function () {
+    if (userCars.length >= 1) game.state.start("CarsList");
+    else {
+      console.log("Loading...");
+    }
+  },
+};
