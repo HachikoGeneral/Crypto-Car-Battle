1354a4311ed24ac3512046b7201a98c1fde04b1b
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-14 00:12:36 +0000

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -25,8 +25,7 @@ var Login = {
         if(NFT_balance == 0) {
           game.state.start('Mint');
         } else {
-          game.state.start('CarsListLoader');
-        
+          game.state.start('CarsListLoader'); 
     }
     })
-  }
+  }}
