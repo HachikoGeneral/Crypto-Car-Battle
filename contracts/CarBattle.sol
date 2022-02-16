a289b38e62125c98c6378ca2608c1a9569de5f0f
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-16 23:40:24 +0000

    Update CarBattle.sol

--- contracts/CarBattle.sol
+++ contracts/CarBattle.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.2;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/token/ERC721/ERC721.sol";
 
 contract CarBattle is ERC721 {
     
