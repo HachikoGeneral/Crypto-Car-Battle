1ae4ecaf5d08de46bf4da9bfce4e6353519642f8
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-18 21:19:42 +0000

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,7 +49,7 @@ var Game = {
     building.body.loadPolygon("collision", "building"); //Load Bounding Box from Physics Editor File
     
      /*Adding outer_weed*/
-    var outer_weed = game.add.sprite(200, 220, "outer_weed");
+    var outer_weed = game.add.sprite(200, 180, "outer_weed");
     game.physics.p2.enable(outer_weed);
     outer_weed.body.kinematic = true; //Building is static
     outer_weed.body.clearShapes(); //Remove standard Bounding Box
