b9643e013e6639af60672787fd2233229b4a7442
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-18 19:44:05 +0000

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,6 @@ var Game = {
       "car",
       `./assets/images/cars/${carLevelWithColor[selectedCar.level]}.png`
     );
-    game.load.spritesheet("outer_weed", "./assets/images/outer_weed.png");
     game.load.spritesheet("building", "./assets/images/building.png");
     game.load.physics("collision", "./assets/js/collision.json");
     game.load.image("trigger", "./assets/images/trigger1.png");
@@ -36,7 +35,6 @@ var Game = {
     /*Create Collision Groups*/
     var carCollisionGroup = game.physics.p2.createCollisionGroup();
     var buildingCollisionGroup = game.physics.p2.createCollisionGroup();
-    var outer_weedCollisionGroup = game.physics.p2.createCollisionGroup();
     game.physics.p2.updateBoundsCollisionGroup();
 
     /*Adding Building*/
