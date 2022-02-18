85f1451d07582af8f8e3977ba3eed75545abd125
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-02-18 21:14:36 +0000

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,7 +81,7 @@ var Game = {
   checkOverlap: function (spriteA, spriteB) {
     var boundsA = spriteA.getBounds();
     var boundsB = spriteB.getBounds();
-    var boundsA = spriteA.getBounds();
+    var boundsB = spriteB.getBounds();
 
     return Phaser.Rectangle.intersects(boundsA, boundsB);
   },
